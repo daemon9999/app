f7bffd003dbfd7a995747581ec8b9e487c5c8282
Murad Masimli <murad.mesimli.99@gmail.com>
2024-09-04 22:05:45 +0400

    fixed

---
+++ client2/server/index.js
@@ -0,0 +1,16 @@
+const path = require("path")
+const express = require("express")
+
+const app = express();
+
+
+const root = path.join(__dirname, "..//dist")
+
+app.use(express.static(root));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '..//dist', 'index.html'));
+});
+
+
+app.listen(3000, () => console.log("Server is running on 3000"))
